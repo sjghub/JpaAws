a5241cebd062a98c76a451628ec3e55dbc1a729c
esfuerzo <dkdldrha@naver.com>
2024-02-23 14:54:26 +0900

    Initial commit

---
+++ src/main/java/ToyProject/JpaAws/JpaAwsApplication.java
@@ -0,0 +1,14 @@
+package ToyProject.JpaAws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class JpaAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ToyProject/JpaAws/JpaAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package ToyProject.JpaAws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
